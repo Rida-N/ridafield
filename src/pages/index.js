afd56722a22973aa06b1115dc499ce711de50261
chou <chou@MacBook-Pro.local>
2020-06-19 13:47:50 +0900

    reconstructe

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>One who makes no mistakes makes nothing at all.</h1>
+    <p>Fragments of technical study notes, works of art, and mind-wandering</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
